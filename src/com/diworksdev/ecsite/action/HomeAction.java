079852fbe3d471b201c07d41753e14590d7fdd97
yunahamazawa <yunahamazawa821@gmail.com>
2025-03-06 23:40:28 +0900

    P28まで（エラーあり）

--- src/com/diworksdev/ecsite/action/HomeAction.java
+++ src/com/diworksdev/ecsite/action/HomeAction.java
@@ -5,6 +5,8 @@ import java.util.Map;
 
 import org.apache.struts2.interceptor.SessionAware;
 
+import com.diworksdev.ecsite.dao.BuyItemDAO;
+import com.diworksdev.ecsite.dto.BuyItemDTO;
 import com.opensymphony.xwork2.ActionSupport;
 public class HomeAction extends ActionSupport implements SessionAware {
 public Map<String, Object> session;
@@ -12,7 +14,7 @@ public Map<String, Object> session;
 public String execute() {
 	String result = "login";
 	if (session.containsKey("login_user_id")) {
-		BuyItemDAO buyItemDAO = new BuyItemDAO ();
+		BuyItemDAO buyItemDAO = new BuyItemDAO();
 		BuyItemDTO buyItemDTO = buyItemDAO.getBuyItemInfo();
 		session.put("id", buyItemDTO.getId());
 		session.put("buyItem_name",buyItemDTO.getItemName());
